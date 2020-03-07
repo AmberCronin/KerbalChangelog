8bae5c4822b8c1440aae260e527da983d21faec5
Benjamin Cronin <bcronin720@gmail.com>
2020-03-07 11:34:50 -0800

    fix for changelog appearing when showChangelog is false

--- Changelog.cs
+++ Changelog.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 namespace KerbalChangelog
 {
-    public class Changelog
-    {
+	public class Changelog
+	{
 		public string modName { get; private set; }
 		public string license { get; private set; } = null;
 		public string author { get; private set; } = null;
 
-		bool showCL = true;
+		public bool showCL { get; private set; } = true;
 
-        List<ChangeSet> changeSets = new List<ChangeSet>();
-        public ChangelogVersion highestVersion
-        {
-            get
-            {
-                changeSets.Sort();
+		List<ChangeSet> changeSets = new List<ChangeSet>();
+		public ChangelogVersion highestVersion
+		{
+			get
+			{
+				changeSets.Sort();
 				try
 				{
 					return changeSets[0].version;
@@ -28,40 +28,45 @@ namespace KerbalChangelog
 					return null;
 				}
 			}
-        }
+		}
 
-        public Changelog(string mn, bool show, List<ChangeSet> cs)
-        {
-            modName = mn;
-            showCL = show;
-            changeSets = cs;
-        }
+		public Changelog(string mn, bool show, List<ChangeSet> cs)
+		{
+			modName = mn;
+			showCL = show;
+			changeSets = cs;
+		}
 
-        public Changelog(ConfigNode cn, UrlDir.UrlConfig cfgDir)
-        {
-			string cfgDirName = cfgDir.ToString();
-            string _modname = "";
-            if (!cn.TryGetValue("modName", ref _modname))
-            {
-                Debug.Log("[KCL] Missing mod name for changelog file in directory: " + cfgDirName);
-                Debug.Log("[KCL] Continuing using directory name as mod name...");
-                modName = cfgDirName;
-            }
-            else
-            {
-                modName = _modname;
-            }
+		public Changelog(ConfigNode cn, UrlDir.UrlConfig cfgDir)
+		{
+			string cfgDirName = cfgDir.url;
+			string _modname = "";
+			if (!cn.TryGetValue("modName", ref _modname))
+			{
+				Debug.Log("[KCL] Missing mod name for changelog file in directory: " + cfgDirName);
+				Debug.Log("[KCL] Continuing using directory name as mod name...");
+				modName = cfgDirName;
+			}
+			else
+			{
+				modName = _modname;
+			}
 
-            if (!cn.TryGetValue("showChangelog", ref showCL))
-            {
-                Debug.Log("[KCL] \"showChangelog\" field does not exist in mod ");
-                Debug.Log("[KCL] Assuming [true] to show changelog, adding field to changelog...");
-                if (!cn.SetValue("showChangelog", false, true)) //creates a new field for the viewing status, setting it to false
-                {
-                    Debug.Log("[KCL] Unable to create 'showChangelog' in directory " + cfgDirName + " (field was missing in file)");
-                }
+			bool _showCL = true;
+			if (!cn.TryGetValue("showChangelog", ref _showCL))
+			{
+				Debug.Log("[KCL] \"showChangelog\" field does not exist in mod ");
+				Debug.Log("[KCL] Assuming [true] to show changelog, adding field to changelog...");
+				if (!cn.SetValue("showChangelog", false, true)) //creates a new field for the viewing status, setting it to false
+				{
+					Debug.Log("[KCL] Unable to create 'showChangelog' in directory " + cfgDirName + " (field was missing in file)");
+				}
 				cfgDir.parent.SaveConfigs();
-            }
+			}
+			else
+			{
+				showCL = _showCL;
+			}
 
 			string _author = "";
 			if(cn.TryGetValue("author", ref _author))
@@ -76,21 +81,22 @@ namespace KerbalChangelog
 
 
 			foreach (ConfigNode vn in cn.GetNodes("VERSION"))
-            {
-                changeSets.Add(new ChangeSet(vn, cfgDirName));
-            }
-        }
+			{
+				changeSets.Add(new ChangeSet(vn, cfgDirName));
+			}
+		}
 
-        public override string ToString()
-        {
-            string ret = modName + "\n";
+		public override string ToString()
+		{
+			string ret = "";
+			ret += modName + "\n";
 			ret += ((author == null) ? "" : "Created by: " + author + "\n");
 			ret += ((license == null) ? "\n" : "Licensed under the " + license + " license\n\n"); //give a double line break here
-            foreach(ChangeSet cs in changeSets)
-            {
-                ret += cs.ToString();
-            }
-            return ret;
-        }
-    }
+			foreach (ChangeSet cs in changeSets)
+			{
+				ret += cs.ToString();
+			}
+			return ret;
+		}
+	}
 }
